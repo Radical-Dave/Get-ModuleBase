5743b19179e5289ad488a55c15f65d522c6a789d
David Walker <dave@radicaldave.com>
2021-02-01 15:04:56 -0600

    updates

--- Get-ModuleBase.ps1
+++ Get-ModuleBase.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 0.2
+.VERSION 0.3
 
 .GUID 01a27130-5e5f-41f7-a699-b6e9546fdede
 
@@ -36,9 +36,18 @@ Get path to ModuleBase of current script
 In dev, there is extra level for solution that is removed
 #>
 $moduleName = $MyInvocation.MyCommand.Module.Name
-if(!$moduleName) {$moduleName = Split-Path $pwd -Leaf}
-$moduleBase = $MyInvocation.MyCommand.Module.ModuleBase
-if(!$moduleBase) {return $pwd}
+if(!$moduleName) {
+    $moduleName = $pwd #Split-Path $pwd -Leaf
+    $srcFolders = "src public private tests"
+    do {
+        if (!$moduleName) {return}
+        $moduleName = (Get-Item $moduleName).Parent.Name
+        if (!$moduleName) {return}
+    } While ($srcFolders.contains($moduleName.ToLower()))
+}
+$moduleBase = "$($MyInvocation.MyCommand.Module.ModuleBase)"
+if(!$moduleBase) {$moduleBase = "$pwd"}
 $ndx = $moduleBase.IndexOf($moduleName)
 if($ndx -ne -1) {$moduleBase = $moduleBase.Substring(0, $ndx + $moduleName.Length)}
+#Write-Output "moduleName:$moduleName"
 return $moduleBase
